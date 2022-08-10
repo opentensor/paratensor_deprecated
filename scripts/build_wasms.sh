5a6f63c82c091b1247f56f94eecee3c28acb7d3d
Ala Shaabana <ala@bittensor.com>
2022-08-10 15:47:50 -0400

    wasm build script fix

--- scripts/build_wasms.sh
+++ scripts/build_wasms.sh
@@ -3,9 +3,9 @@
 # Run this script from the parent directory!
 
 # Obtain Wasm runtime validation function
-./target/release/parachain-collator export-genesis-wasm --chain ../specs/rococo-local-parachain-2000-raw.json > para-2000-wasm
+./target/release/parachain-collator export-genesis-wasm --chain ./specs/rococo-local-parachain-2000-raw.json > ./wasms/para-2000-wasm
 
 # Generate a parachain genesis state
-./target/release/parachain-collator export-genesis-state --chain ../specs/rococo-local-parachain-2000-raw.json > para-2000-genesis
+./target/release/parachain-collator export-genesis-state --chain ./specs/rococo-local-parachain-2000-raw.json > ./wasms/para-2000-genesis
 
 
