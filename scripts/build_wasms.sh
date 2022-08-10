78a8922fa06cd4c05cd2d2b241ff76a91ca6bc28
Ala Shaabana <ala@bittensor.com>
2022-08-10 15:47:23 -0400

    wasm build script comments

--- scripts/build_wasms.sh
+++ scripts/build_wasms.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # This script builds the genesis head and wasm files for registering a parachain
+# Run this script from the parent directory!
 
 # Obtain Wasm runtime validation function
-../target/release/parachain-collator export-genesis-wasm --chain ../specs/rococo-local-parachain-2000-raw.json > para-2000-wasm
+./target/release/parachain-collator export-genesis-wasm --chain ../specs/rococo-local-parachain-2000-raw.json > para-2000-wasm
 
 # Generate a parachain genesis state
-../target/release/parachain-collator export-genesis-state --chain ../specs/rococo-local-parachain-2000-raw.json > para-2000-genesis
+./target/release/parachain-collator export-genesis-state --chain ../specs/rococo-local-parachain-2000-raw.json > para-2000-genesis
 
 
