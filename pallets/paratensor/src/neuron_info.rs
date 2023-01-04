8cddad60485d6ed204d75878325b03803e68ee94
Cameron Fairchild <cameron@opentensor.ai>
2023-01-04 00:30:34 -0500

    add serde_bytes

--- pallets/paratensor/src/neuron_info.rs
+++ pallets/paratensor/src/neuron_info.rs
@@ -4,16 +4,17 @@ use frame_support::storage::IterableStorageDoubleMap;
 use frame_support::pallet_prelude::{Decode, Encode};
 use frame_support::inherent::Vec;
 
-#[derive(Decode, Encode, Serialize, Deserialize)]
+#[derive(Decode, Encode, Serialize, Deserialize, PartialEq, Eq, Clone, Debug)]
 #[serde(deny_unknown_fields)]
-pub struct NeuronInfo {//<T:Config> {
-    //hotkey: T::AccountId,
-    //coldkey: T::AccountId,
+pub struct NeuronInfo {
+    hotkey: DeAccountId,
+    coldkey: DeAccountId,
     uid: u16,
     netuid: u16,
     active: bool,
     axon_metadata: AxonMetadata,
-    //stake: Vec<(T::AccountId, u64)>, // map of coldkey to stake on this neuron/hotkey (includes delegations)
+    #[serde(with = "tuple_vec_map")]
+    stake: Vec<(DeAccountId, u64)>, // map of coldkey to stake on this neuron/hotkey (includes delegations)
     rank: u16,
     emission: u64,
     incentive: u16,
@@ -28,8 +29,23 @@ pub struct NeuronInfo {//<T:Config> {
     pruning_score: u16
 }
 
+#[derive(Decode, Encode, Serialize, Deserialize, PartialEq, Eq, Clone, Debug)]
+struct DeAccountId { // allows us to de/serialize the account id as a u8 vec
+    #[serde(with = "serde_bytes")]
+    id: Vec<u8>
+}
+
+impl From<Vec<u8>> for DeAccountId {
+    fn from(v: Vec<u8>) -> Self {
+        DeAccountId {
+            id: v.clone()
+        }
+    }
+}
+
+
 impl<T: Config> Pallet<T> {
-	pub fn get_neurons(netuid: u16) -> Vec<NeuronInfo> {//<T>> {
+	pub fn get_neurons(netuid: u16) -> Vec<NeuronInfo> {
         if !Self::if_subnet_exist(netuid) {
             return Vec::new()
         }
@@ -67,20 +83,21 @@ impl<T: Config> Pallet<T> {
             let weights = Weights::<T>::get( netuid, uid_i as u16 );
             let bonds = Bonds::<T>::get( netuid, uid_i as u16 );
             
-            let mut stakes = Vec::<(T::AccountId, u64)>::new();
+            let mut stakes = Vec::<(DeAccountId, u64)>::new();
             for ( coldkey, stake ) in < Stake<T> as IterableStorageDoubleMap<T::AccountId, T::AccountId, u64> >::iter_prefix( hotkey.clone() ) {
-                stakes.push( (coldkey.clone(), stake) );
+                stakes.push( (coldkey.clone().encode().into(), stake) );
             }
+
             let stake = stakes;
 
             let neuron = NeuronInfo {
-                //hotkey,
-                //coldkey,
+                hotkey: hotkey.clone().encode().into(),
+                coldkey: coldkey.clone().encode().into(),
                 uid,
                 netuid,
                 active,
                 axon_metadata,
-                //stake,
+                stake,
                 rank,
                 emission,
                 incentive,
