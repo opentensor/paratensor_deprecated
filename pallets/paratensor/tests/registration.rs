64fc17dabedab2da2d7484e17fb1edd13ebe3b16
opentaco <opentaco@protonmail.com>
2023-01-06 10:52:10 +0200

    Remove warning

--- pallets/paratensor/tests/registration.rs
+++ pallets/paratensor/tests/registration.rs
@@ -306,7 +306,7 @@ fn test_registration_get_neuron_metadata() {
 		assert_ok!(ParatensorModule::register(<<Test as Config>::Origin>::signed(hotkey_account_id), netuid, block_number, nonce0, work0, hotkey_account_id, coldkey_account_id));
 		//
 		//let neuron_id = ParatensorModule::get_uid_for_net_and_hotkey(netuid, &hotkey_account_id);
-		let neuron_uid = ParatensorModule::get_uid_for_net_and_hotkey( netuid, &hotkey_account_id ).unwrap();
+		// let neuron_uid = ParatensorModule::get_uid_for_net_and_hotkey( netuid, &hotkey_account_id ).unwrap();
 		let neuron: AxonInfoOf = ParatensorModule::get_axon_info( &hotkey_account_id );
 		assert_eq!(neuron.ip, 0);
 		assert_eq!(neuron.version, 0);
